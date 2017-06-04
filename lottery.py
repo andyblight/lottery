e9a8a0f617a434132406cdc1b6470e49967b3bad
Andy Blight <andy.blight@gmail.com>
2017-06-04 19:20:57 +0100

    Refactored into lottery classes

--- lottery.py
+++ lottery.py
@@ -1,34 +1,45 @@
 #!/usr/bin/python3.6
 
 """
-TODO
+GOAL
+Generate statistics on the balls and the draws that can be fed into an
+algorithm used to predict the next draw. 
+Use 5 months of the 6 months of data to predict the remaining months draws. 
+
+NOTES
+Predicting the EuroMillions lucky star balls should be simplest with a 2 out 
+of 12 choice.  Focus on that first.
+Does the EuroMillions use multiple machines/balls sets?
+ 
+TASKS
+Print out rolling n days frequency figures.
+Print out the most likely numbers for the next n draws.  
 
 """
 
-import csv
 import datetime
 
 from lottery_results import LotteryResults
+from lottery_utils import frequency
 
+def frequency_in_date_range(results, date_from, date_to):
+    """ TODO """
+    print("From", date_from, "to", date_to)
+    balls = results.get_lottery().get_balls_in_date_range(date_from, date_to)
+    frequency(50, balls[0])
+    frequency(12, balls[1])
 
 def process_data(results):
     """ TODO """
-    latest_date = results.get_latest_date()
-    earliest_date = latest_date + datetime.timedelta(days=-30)
-    results.frequency_in_date_range(earliest_date, latest_date)
+    date_range = results.get_lottery().get_date_range()
+    latest_date = date_range[1]
+    earliest_date = latest_date + datetime.timedelta(days=-60)
+    frequency_in_date_range(results, earliest_date, latest_date)
 
 def run(filename):
     """ Reads the data from the given file into the results instance """
     results = LotteryResults()
-    with open(filename, newline='') as csvfile:
-        filereader = csv.reader(csvfile, delimiter=',', quotechar='|')
-        ignore_header = True
-        for row in filereader:
-            if ignore_header:
-                results.parse_header(row)
-                ignore_header = False
-                continue
-            results.parse_row(row)
+    results.load_file(filename)
     process_data(results)
 
 if __name__ == "__main__":
--- lottery_results.py
+++ lottery_results.py
@@ -1,42 +1,87 @@
 #!/usr/bin/python3.6
 
-"""
-TODO
-"""
+from _thread import _count
 import collections
+import csv
 import sys
 
 from lottery_utils import convert_str_to_date, frequency
+
+
+class SetOfBalls:
+    """ Information about the set of balls. """
+    _count = 0
+
+    def __init__(self, count):
+        """ Initialises the class. """
+        _count = count
+
+    def get_count(self):
+        """ Return the number of balls in the set. """
+        return _count
+
+
+class Lottery:
+    """ 
+    The base class for all lotteries.
+    This class implements the functions for a single set of balls (the most
+    common type of lottery). 
+    """
+    _name = ""
+    results = []
+    num_rows = 0
+
+    def __init__(self):
+        """ Initialises the class. """
+        self._name = 'default'
+        self.results = []
+        self.num_rows = 0
+        self._main_balls = SetOfBalls(10)
+
+    def get_name(self):
+        """ Returns the name of the lottery. """
+        return self._name
+
+    def check_header(self, row):
+        """ Returns True if the header row is for this lottery """
+        return False
+
+    def get_set_of_balls(self):
+        """ Returns a set of balls.. """
+        return self._set_of_balls
+
+    def get_date_range(self):
+        """ Returns a tuple containing the earliest and latest dates in the results. """
+        return (0, 0)
+
+    def get_balls_in_date_range(self, date_from, date_to):
+        """ Gets the a tuple containing the sets of balls in the give date 
+        range. """
+        return (0)
 
 EuroMillionsRow = collections.namedtuple('EuroMillionsRow', \
         ['draw_date', 'main_1', 'main_2', 'main_3', 'main_4', 'main_5', \
         'lucky_1', 'lucky_2'])
 
-class LotteryResults:
-    """ The base class for holding lottery results that have been parsed from
-    a .csv file.
-    """
-    results = []
-    type = ""
-    num_rows = 0
+
+class LotteryEuroMillions(Lottery):
+    """ The Euro Millions lottery. """
+    _main_balls = SetOfBalls(50)
+    _lucky_star_balls = SetOfBalls(12)
 
     def __init__(self):
-        self.results = []
-        self.type = ""
-        self.num_rows = 0
+        self._name = "EuroMillions"
+        self._sets_of_balls = 2
 
-    def parse_header(self, row):
-        """ Parse the header row to work out the file type. """
-        self.type = ""
-        if str(row[6]) == 'Lucky Star 1':
-            self.type = 'euro'
-        elif str(row[7]) == 'Bonus Ball':
-            self.type = 'lotto'
-        else:
-            print("Unknown file type")
-            sys.exit(1)
+    def check_header(self, row):
+        """ Returns True if the header row is for this lottery """
+        return str(row[6]) == 'Lucky Star 1'
 
-    def parse_euro_millions_row(self, row):
+    def get_balls(self):
+        """ Returns a set of balls.. """
+        return (self._main_balls, self._lucky_star_balls)
+
+    def parse_row(self, row):
         """ Read row data and copy into EuroMillions tuple """
         euro = EuroMillionsRow(convert_str_to_date(str(row[0])), \
                                int(row[1]), int(row[2]), int(row[3]), \
@@ -45,42 +90,67 @@ class LotteryResults:
         self.results.append(euro)
         self.num_rows += 1
 
-    def parse_row(self, row):
-        """ Parse row data and append """
-        if self.type == 'euro':
-            self.parse_euro_millions_row(row)
-        elif self.type == 'lotto':
-            print("TODO")
-        else:
-            print("Unknown file type")
-            sys.exit(1)
-
-    def get_latest_date(self):
-        """ The latest date is the first element on the first row of results """
-        # print("Latest: ", self.results[0].draw_date)
-        return self.results[0].draw_date
-
-    def get_main_balls_in_row(self, row, main_balls):
-        """ Appends all main balls to the given list """
-        main_balls.append(row.main_1)
-        main_balls.append(row.main_2)
-        main_balls.append(row.main_3)
-        main_balls.append(row.main_4)
-        main_balls.append(row.main_5)
-
-    def get_lucky_stars_in_row(self, row, lucky_stars):
-        """ Appends all lucky star balls to the given list """
-        lucky_stars.append(row.lucky_1)
-        lucky_stars.append(row.lucky_2)
-
-    def frequency_in_date_range(self, date_from, date_to):
-        """ TODO """
-        print("From", date_from, "to", date_to)
+    def get_balls_in_date_range(self, date_from, date_to):
+        """ Gets the a tuple containing the sets of balls in the give date 
+        range. """
         main_balls = []
         lucky_stars = []
         for row in self.results:
             if row.draw_date >= date_from and row.draw_date <= date_to:
-                self.get_main_balls_in_row(row, main_balls)
-                self.get_lucky_stars_in_row(row, lucky_stars)
-        frequency(50, main_balls)
-        frequency(12, lucky_stars)
+                """ Appends all main balls to the given list """
+                main_balls.append(row.main_1)
+                main_balls.append(row.main_2)
+                main_balls.append(row.main_3)
+                main_balls.append(row.main_4)
+                main_balls.append(row.main_5)
+                """ Appends all lucky star balls to the given list """
+                lucky_stars.append(row.lucky_1)
+                lucky_stars.append(row.lucky_2)
+        return (main_balls, lucky_stars)
+
+    def get_date_range(self):
+        """ Returns a tuple containing the earliest and latest dates in the results. """
+        earliest = self.results[0].draw_date
+        latest = self.results[0].draw_date
+        return (earliest, latest)
+
+
+class LotteryResults:
+    """
+    The LotteryResults class reads a lottery results CSV file into an internal 
+    cache and provides methods to access the data in the internal cache. 
+.   """
+    _lottery = Lottery()
+    _euro_millions = LotteryEuroMillions()
+
+    def __init__(self):
+        self.type = ""
+
+    def parse_header(self, row):
+        """ Parse the header row to work out the file type. """
+        self.type = ""
+        if self._euro_millions.check_header(row):
+            self._lottery = self._euro_millions
+        elif str(row[7]) == 'Bonus Ball':
+            print("FIXME: Using default lottery")
+        else:
+            print("Using default lottery")
+
+    def parse_row(self, row):
+        """ Parse row data and append """
+        self._lottery.parse_row(row)
+
+    def load_file(self, filename):
+        with open(filename, newline='') as csvfile:
+            filereader = csv.reader(csvfile, delimiter=',', quotechar='|')
+            ignore_header = True
+            for row in filereader:
+                if ignore_header:
+                    self.parse_header(row)
+                    ignore_header = False
+                    continue
+                self._lottery.parse_row(row)
+
+    def get_lottery(self):
+        """ Returns the lottery instance. """
+        return self._lottery
