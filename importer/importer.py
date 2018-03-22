a88429d4c614895fe325ac48b82c02621bc69a37
Andy Blight <andy.blight@gmail.com>
2018-03-22 09:30:11 +0000

    Gets correct info and writes it all to a log file

--- importer/importer.py
+++ importer/importer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3.6
-
 """
 This file imports CSV data from the merseyworld web site.  Pages are:
 Lotto:
@@ -14,7 +13,12 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
 LOG_FILE = "importer.log"
-URL = "http://lottery.merseyworld.com/cgi-bin/lottery?days=20&Machine=Z&Ballset=0&order=0&show=1&year=-1&display=CSV"
+MERSEYWORLD_BASE_URL = "http://lottery.merseyworld.com/cgi-bin/lottery?"
+LOTTO = "days=2&Machine=Z&Ballset=0&order=0&show=1&year=-1&display=CSV"
+LOTTO_URL = MERSEYWORLD_BASE_URL + LOTTO
+EUROMILLIONS = "days=20&Machine=Z&Ballset=0&order=0&show=1&year=-1&display=CSV"
+EUROMILLIONS_URL = MERSEYWORLD_BASE_URL + EUROMILLIONS
+
 
 def handle_parameters():
     """ Process the command line arguments.  Returns the values of the
@@ -26,6 +30,7 @@ def handle_parameters():
     parser.add_argument('-v', '--verbose', help='''verbose output.''')
     return parser.parse_args()
 
+
 def setup_logging(args):
     """ Set up logging. """
     logging.basicConfig(filename=LOG_FILE, filemode='w', level=logging.DEBUG)
@@ -33,13 +38,25 @@ def setup_logging(args):
     logging.info(args)
 
 
+def fetch_and_process(url):
+    """ Todo """
+    logging.info("Fetching info from: %s", url)
+    content = urlopen(url).read()
+    soup = BeautifulSoup(content, "lxml")
+    print(soup.prettify())
+    logging.info(soup.prettify())
+    # TODO
+    # Write to CSV file.
+    # Append results to one big file or keep for a year?
+
+
 def run():
     """ Todo """
     args = handle_parameters()
     setup_logging(args)
-    content = urlopen(URL).read()
-    soup = BeautifulSoup(content, "lxml")
-    print(soup.prettify())
+    fetch_and_process(LOTTO_URL)
+    fetch_and_process(EUROMILLIONS_URL)
+
 
 if __name__ == "__main__":
     run()
